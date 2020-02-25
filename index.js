220eb715d9a2290f2ad4d1db2d9f92f9f41844cc
Eric Clark <eclark0013@gmail.com>
2020-02-25 10:51:50 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+function wakeDog(dogName, dogBreed) {
+    let statement = `Wake ${dogName} the ${dogBreed}`;
+    console.log(statement);
+    return statement;
+  }
+
+function leashDog(dogName, dogBreed) {
+    let statement = `Leash ${dogName} the ${dogBreed}`;
+    console.log(statement);
+    return statement;
+  }
+
+function walkToPark(dogName, dogBreed) {
+    let statement = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(statement);
+    return statement;
+  }
+
+function throwFrisbee(dogName, dogBreed) {
+    let statement = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    console.log(statement);
+    return statement;
+  }
+
+function walkHome(dogName, dogBreed) {
+    let statement = `Walk home with ${dogName} the ${dogBreed}`;
+    console.log(statement);
+    return statement;
+  }
+
+function unleashDog(dogName, dogBreed) {
+    let statement = `Unleash ${dogName} the ${dogBreed}`;
+    console.log(statement);
+    return statement;
+  }
+
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+    let activitiesCompleted = []
+    for (let i = 0; i < routine.length; i++) {
+        activitiesCompleted.push(routine[i](dogName, dogBreed));
+        }
+    return activitiesCompleted
+}
