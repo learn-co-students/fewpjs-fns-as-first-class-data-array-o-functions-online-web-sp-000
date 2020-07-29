a23117b62338d59da23d871779a61e6ce5558514
Alvin Coulson <aaacoulson@gmail.com>
2020-07-29 10:11:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var wakeDog = function(dogName, dogBreed) {
+let wakeDog = function(dogName, dogBreed) {
     return `Wake ${dogName} the ${dogBreed}`
 }
 
-var leashDog = function(dogName, dogBreed) {
+let leashDog = function(dogName, dogBreed) {
     return `Leash ${dogName} the ${dogBreed}`
 }
 
-var walkToPark = function(dogName, dogBreed) {
+let walkToPark = function(dogName, dogBreed) {
     return `Walk to the park with ${dogName} the ${dogBreed}`
 }
 
-var throwFrisbee = function(dogName, dogBreed) {
+let throwFrisbee = function(dogName, dogBreed) {
     return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
 
-var walkHome = function(dogName, dogBreed) {
+let walkHome = function(dogName, dogBreed) {
     return `Walk home with ${dogName} the ${dogBreed}`
 }
 
-var unleashDog = function(dogName, dogBreed) {
+let unleashDog = function(dogName, dogBreed) {
     return `Unleash ${dogName} the ${dogBreed}`
 }
 
